dc697c6aa562391e1102ee1124799720f692a10b
William <p23wclegg@fairfieldprep.org>
2022-09-27 10:47:30 -0400

    Lab-3-2 Error message lab

---
+++ Lab-3-2.py
@@ -0,0 +1,7 @@
+#Lab-3-2
+#int(a) #Name error 
+#int('a') #value error
+#'a'+2 #Type error, adding a number to a letter
+import date #module not found error 
+#print("im a happy camper!)
+#Syntax error 
