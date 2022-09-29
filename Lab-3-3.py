2cb3b67345c9bb7ed784e4ea34eca13dde2450e6
William <p23wclegg@fairfieldprep.org>
2022-09-29 12:11:25 -0400

    Finished 3-3 yesterday but didn't have time to submit

---
+++ Lab-3-3.py
@@ -0,0 +1,13 @@
+#Lab-3-3
+import random
+a=random.randint(1,100) 
+random.randint(1,100)
+#97
+
+random.seed(58)
+print(a)
+print (random.random())
+#0.5807571649339567
+
+b=random.randrange(0,100,2)
+print(b)
---
+++ Lab-3-4.py
@@ -0,0 +1,13 @@
+#Lab-3-4
+import math
+math.ceil(7.15)#8 
+math.floor(19.156) #19
+math.fabs(-42) #42
+math.pow(2,3) #8
+math.log10(100)
+math.sqrt(9)#3 
+math.isqrt(2)#1
+math.factorial(5)#5
+math.gcd(111111111, 12321)#1
+print (math.sin(0))
+
