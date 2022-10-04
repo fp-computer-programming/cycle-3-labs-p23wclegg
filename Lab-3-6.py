60071a254f8a7a3319db5f896111bb5a096a5993
William <p23wclegg@fairfieldprep.org>
2022-10-04 08:55:56 -0400

    3-6

---
+++ Lab-3-6.py
@@ -0,0 +1,15 @@
+#Lab-3-6
+
+var_1 = "Fairfield"
+var_2 = "Prep"
+print(var_1+" " +var_2)
+
+a='_'
+print (20*a)
+
+b="I\'m loving this short story I\'m reading, \"The fall of the House of Usher\""
+print(len(b))
+
+c='apple'
+character=c[3]
+print (character)
